42085d324e85326b53fa9c048dc0ebf25e9e4d5e
sarah ogrady <sarahfrancesogrady@gmail.com>
2021-09-24 17:31:39 +0100

    add data targets and stimulus for reloading the movie choices

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,5 +5,12 @@ class MoviesController < ApplicationController
     if params[:query].present?
       @movies = @movies.where('title ILIKE ?', "%#{params[:query]}%")
     end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'movies/list', locals: { movies: @movies }, formats: [:html] }
+      # I understnad this is re rendering the partial, giving the @movies instance variable
+      # what is the respond_to iteration and what is 'format'
+    end
   end
 end
--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -1,13 +1,41 @@
-import { Controller } from "stimulus"
+// import { Controller } from "stimulus"
+
+// export default class extends Controller {
+//   static targets = ['form', 'input', 'list'];
+//   connect() {
+//     console.log(this.formTarget);
+//     console.log(this.inputTarget);
+//   }
+
+//   updateList(event) {
+//     // console.log('send request in AJAX');
+//     const url = this.formTarget.action
+//     // console.log(url) -> shows us where the form is being sent
+//     // for us here page is being re-rendered: movies_path
+//     fetch(`${url}?query=${this.inputTarget.value}`, { headers: { 'Accept': 'text/plain'} })
+//       // { headers: { 'Accept': 'text/plain'} } -> WHAT DOES THIS DO????
+//       .then(response => response.text())
+//       .then((data) => {
+//         this.listTarget.outerHtml = data;
+//       });
+//   }
+// }
+
+import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['form', 'input'];
+  static targets = ['form', 'input', 'list'];
+
   connect() {
     console.log(this.formTarget);
     console.log(this.inputTarget);
   }
-
-  updateList() {
-
+  updateList(event) {
+    const url = this.formTarget.action;
+    fetch(`${url}?query=${this.inputTarget.value}`, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data;
+      });
   }
 }
