c2d9249470cb3faaa8bb81077ab595b08cd47ea0
sarah ogrady <sarahfrancesogrady@gmail.com>
2021-10-06 17:38:28 +0100

    add data controller, target and action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,11 +8,22 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.text
-      # format.text { render partial: 'movies/list', locals: { movies: @movies }, formats: [:html] }
+      format.text { render partial: 'movies/list', locals: { movies: @movies }, formats: [:html] }
       # format.json { render json: {hi: 'lucien'} }
       # I understnad this is re rendering the partial, giving the @movies instance variable
       # what is the respond_to iteration and what is 'format'
     end
   end
+
+  # def update
+  #   @movie = Movie.find(params[:id])
+  #   @movie.update(movie_params)
+  #   repspond
+  # end
+
+  # private
+
+  # def movie_params
+  #   params.require(:movie).permit(:title, :year)
+  # end
 end
